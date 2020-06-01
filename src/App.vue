c30ced56c29efa467918d13b7982925e241d0ff9
Wen Tao <wentao75@gmail.com>
2020-06-01 18:09:40 +0800

    添加Element库

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <template>
     <div id="app">
+        <div>
+            <p>
+                If Element is successfully added to this project, you'll see an
+                <code v-text="'<el-button>'"></code>
+                below
+            </p>
+            <el-button>el-button</el-button>
+        </div>
         <router-view />
     </div>
 </template>
+
+<script>
+export default {
+    name: "app",
+    components: {}
+};
+</script>
 
 <style>
 html,
@@ -14,5 +29,12 @@ body,
     margin: 0px;
     width: 100%;
     height: 100%;
+}
+#app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import Echarts from "echarts";
+import "./plugins/element.js";
 
 Vue.config.productionTip = false;
 Vue.prototype.echarts = Echarts;
